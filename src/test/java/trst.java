6a752246fc9b42da852e702b2769cccb35404735
DesktopPC <befanalex2@gmail.com>
2025-09-06 13:32:23 -0500

    V3

--- src/test/java/trst.java
+++ src/test/java/trst.java
@@ -2,5 +2,6 @@ public class trst {
     public static void main(String[] args) {
         System.out.println("Hello world V1");
         System.out.println("Hello world V2");
+        System.out.println("Hello world V3");
     }
 }
